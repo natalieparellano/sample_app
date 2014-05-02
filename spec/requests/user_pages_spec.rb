b0e4a5ba7e489133d5f8637f8f1197abc37a8cea
natalieparellano <natalie.p.arellano@gmail.com>
2014-05-02 13:59:50 -0400

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,6 +32,16 @@ describe "User pages" do
       it "should create a user" do 
         expect { click_button "Create my account" }.to change(User, :count).by(1)
       end 
+
+      describe "after saving the user" do
+        before { click_button submit }
+
+        let(:user) { User.find_by(email: 'mooky@cat.com') }
+
+        it { should have_link("Sign out") }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end  
     end 
   end
 
